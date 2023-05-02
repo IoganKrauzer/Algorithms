0fef7278e305c51c70415b507a083ef933f41013
Sergey <asmcm88@gmail.com>
2023-05-02 05:49:57 +0300

    Training

---
+++ Insert/Program.cs
@@ -0,0 +1,70 @@
+﻿Console.Clear();
+int[] arr = { 5, 2, 9, 3, 4, 1 };
+int len = arr.Length;
+int temp = 0;
+int sortedSize = 0;
+// int index = 1;
+
+for (int i = 0; i < len; i++)
+{
+    Console.Write("| " + arr[i] + " |");
+}
+Console.WriteLine("\n");
+
+
+
+while (sortedSize < len - 1)
+{
+    int index = 1;
+    index = index + sortedSize;
+
+    if (arr[index] < arr[index - 1])
+    {
+        temp = arr[index - 1];
+        arr[index - 1] = arr[index];
+        arr[index] = temp;
+
+
+        if ((index - 1) == 0)
+        {
+            sortedSize++;
+            continue;
+        }
+        else
+        {
+            while (index - 1 != 0)
+            {
+                index--;
+                if (arr[index] < arr[index - 1])
+                {
+                    temp = arr[index - 1];
+                    arr[index - 1] = arr[index];
+                    arr[index] = temp;
+                }
+            }
+            sortedSize++;
+            continue;
+        }
+    }
+    else
+    {
+        sortedSize++;
+        continue;
+    }
+}
+
+
+
+
+
+
+for (int i = 0; i < len; i++)
+{
+    Console.Write("| " + arr[i] + " |");
+}
+
+
+
+
+
+
---
+++ Puzyrek/Program.cs
@@ -0,0 +1,89 @@
+﻿Console.Clear();
+int[] arr = { 5, 3, 4, 2, 1 };
+int len = arr.Length;
+int count = 0;
+int temp = 0;
+
+for (int j = 0; j < len; j++)                      // Для просмотра массива
+{
+    Console.Write("| " + arr[j] + " |");
+}
+Console.WriteLine("\n");
+
+while (count < len - 1)
+{
+   int index = 0;
+    while (index < len - 1 - count)
+    {
+        if (arr[index] > arr[index + 1])
+        {
+            temp = arr[index];
+            arr[index] = arr[index + 1];
+            arr[index + 1] = temp;
+        }
+        index++;
+    }
+    count++;
+}
+
+for (int j = 0; j < len; j++)       // Для просмотра отсортированного массива
+{
+    Console.Write("| " + arr[j] + " |");
+}
+
+
+
+
+// int[] array = new int[5];
+// int len = array.Length;
+// int[] arr = new int[5];
+// int l = arr.Length;
+// int temp = 0;
+// int count = 0;
+
+
+
+
+// for (int i = 0; i < len; i++)
+// {
+//     array[i] = new Random().Next(1, 99);
+//     Console.Write("|" + array[i] + "| ");
+// }
+// Console.WriteLine();
+// Console.WriteLine();
+
+// for (int j = 0; j < l; j++)
+// {
+//     arr[j] = array[j];
+
+//     Console.Write("|" + arr[j] + "| ");
+// }
+
+// //  int mMax = arr[0];
+
+
+
+// while (count < l)
+// {
+
+// for (int k = 0; k < l; k++)
+// {
+//     int m = 1;
+//     m++;
+//     if (arr[k] > arr[m])
+//     {
+//         temp = arr[k];
+//         arr[k] = arr[m];
+//         arr[m] = temp;
+//     }
+
+// }
+
+// count++;
+// }
+// for (int s = 0; s < l; s++)
+// {
+//     Console.WriteLine();
+//     Console.WriteLine();
+//     Console.Write("/" + arr[s] + "/ ");
+// }
