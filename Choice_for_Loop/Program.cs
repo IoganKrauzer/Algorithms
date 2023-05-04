c7938a474505715e80697fabb6795900f47384d1
Sergey <asmcm88@gmail.com>
2023-05-04 15:34:17 +0300

    Update2

--- Choice_for_Loop/Program.cs
+++ Choice_for_Loop/Program.cs
@@ -2,11 +2,11 @@
 int[] arr = { 2, 7, 9, 2, 4, 5, 1, 12, 17, 18, 16 };
 int len = arr.Length;
 
-for (int sortCount = 0; sortCount < len; sortCount++)
+for (int sortCount = 0; sortCount < len -1; sortCount++)
 {
     int temp = 0;
-    int min = 0;
-    min += sortCount;
+    int min = 1;
+    min = min + sortCount;
     for (int index = 1; index < len - 1; index++)
     {
         if (arr[index] > arr[index + 1] && arr[min] > arr[index + 1])
